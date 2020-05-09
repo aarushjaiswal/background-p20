29cddee2bfa49680e6b2419873dc29bc034c3f20
aarushjaiswal <64395032+aarushjaiswal@users.noreply.github.com>
2020-05-09 17:25:57 +0530

    updated rgb values

--- sketch.js
+++ sketch.js
@@ -8,39 +8,11 @@ function setup(){
 }
 
 function draw(){
-  if (b>205){
-    b=b/random(2,5)
-  }
-  
-  if (r>205){
-    r=r/random(1,5)
-  }
-
-  if (g>205){
-    g=g/random(1,5)
-  }
-
-  if(r<20){
-    r=r+random(5,30)
-  }
-
-  if(g<20){
-    g=g+random(5,30)
-  }
-
-  if(b<20){
-    b=b+random(5,30)
-  }
-
-  if (r=g){
-    b=(2*r+g)/2
-  }
-  
-  r = map(mouseX, 0, b*4/3, 0, 1550)
-  g = map(mouseY, 0, b*5/4,0, 675)
+  r = map(mouseX, 0, 1500, 0, 255)
+  g = map(mouseY, 0, 600, 0, 255)
   b = (r + g)
 
-  background(r,2*g,b/2);
+  background(r,g,b);
 
   fill("black")
   ellipse(mouseX, mouseY, 20, 20)
